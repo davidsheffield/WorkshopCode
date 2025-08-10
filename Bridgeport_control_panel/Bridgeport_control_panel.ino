791a44cf8738fd455937e3abfacf296a38d8c1c3
David Sheffield <david@davidsheffield.net>
2025-08-09 20:58:06 -0400

    Display speed on four-digit, seven-segment display

--- Bridgeport_control_panel/Bridgeport_control_panel.ino
+++ Bridgeport_control_panel/Bridgeport_control_panel.ino
@@ -7,7 +7,7 @@
 ** the supplied frequency. The maximum operation frequency is 140 Hz (10 V).
 ** That signal is converted to 0-5V by using a voltage divider
 ** and an MCP6002 op-amp. The cutoff frequency of the low-pass filter
-** is 15.9 Hz.
+** is 15.9 Hz (a period of 62.8 ms).
 **
 ** Vin --- 100k ------------------ OPAMP +
 **                |     |                   OPAMP out --- VFD_PIN
@@ -25,8 +25,13 @@
 **
 *******************************************************************************/
 
+#include <TM1637Display.h>
+
+// 1 to enable debug output to serial
+#define DEBUG 0
+
 // Constant for the backgear ratio
-const float BACKGEAR_RATIO = 8.3;
+const double BACKGEAR_RATIO = 8.3;
 
 // Analogue pin for the VFD speed signal
 #define VFD_PIN A0
@@ -39,9 +44,21 @@ const float BACKGEAR_RATIO = 8.3;
 #define CLK 2
 #define DIO 3
 
+// Create display object
+TM1637Display display(CLK, DIO);
+
 // Set frequency range
 bool high_range = true;
 bool last_button_state = false;
+
+// Check for button presses every 10 ms but the display only needs to be updated
+// every 1 s. Empirically determined such that
+// num_of_checks * button_check_interval ~= 1 s
+int button_check_interval = 10; // check for button press every 10 ms
+int num_of_checks = 95; // Number of checks before updating display
+int num_of_checks_done = num_of_checks; // Number of checks left
+double sum_of_speeds = 0.0; // Sum up num_of_checks speeds to get the average
+
 
 void setup() {
     // Set pin modes
@@ -50,15 +67,19 @@ void setup() {
     pinMode(CLK, OUTPUT);
     pinMode(DIO, OUTPUT);
 
+    // Set display brightness (0-7)
+    display.setBrightness(5);
+
     // initialize serial communication at 9600 bits per second:
     Serial.begin(9600);
 }
 
+
 void loop() {
     delay(10); // Low pass filter has a cutoff period of 63 ms
 
-    int range_button = digitalRead(RANGE_BUTTON_PIN);
     // Check the range button and change the range if it is a new press
+    int range_button = digitalRead(RANGE_BUTTON_PIN);
     if (range_button == LOW && !last_button_state) {
         high_range = !high_range;
         last_button_state = true;
@@ -69,19 +90,39 @@ void loop() {
     // Read the VFD signal
     int vfd_value = analogRead(VFD_PIN);
     // Convert to speed [RPM]
-    float fspeed = (vfd_value / 1023.0) * 4025.0;
-    fspeed = high_range ? fspeed : fspeed / BACKGEAR_RATIO;
-    int speed = (int)fspeed;
+    double speed = ((double)vfd_value / 1023.0) * 4025.0;
+    speed = high_range ? speed : speed / BACKGEAR_RATIO;
+    sum_of_speeds += speed;
+
+    if (num_of_checks_done-- == 0) {
+        double average_speed = sum_of_speeds / (double)num_of_checks;
+        sum_of_speeds = 0.0;
+        num_of_checks_done = num_of_checks;
+        int display_speed = (int)average_speed;
+        display.showNumberDec(display_speed);
+
+        // Write to serial for debugging
+        if (DEBUG) {
+            Serial.print("Speed: ");
+            Serial.print(average_speed);
+            Serial.print(" Display Speed: ");
+            Serial.println(display_speed);
+        }
+    }
 
     // Write to serial for debugging
-    Serial.print("RANGE BUTTON: ");
-    Serial.print(range_button);
-    Serial.print("  last_button_state: ");
-    Serial.print(last_button_state);
-    Serial.print("  Range: ");
-    Serial.print(high_range ? "High" : "Low");
-    Serial.print("  VFD: ");
-    Serial.print(vfd_value);
-    Serial.print("  Speed: ");
-    Serial.println(speed);
+    if (DEBUG) {
+        Serial.print("Range: ");
+        Serial.print(high_range);
+        Serial.print(" RANGE BUTTON: ");
+        Serial.print(range_button);
+        Serial.print(" last_button_state: ");
+        Serial.print(last_button_state);
+        Serial.print(" VFD: ");
+        Serial.print(vfd_value);
+        Serial.print(" Speed: ");
+        Serial.print(speed);
+        Serial.print(" Sum of Speeds: ");
+        Serial.println(sum_of_speeds);
+    }
 }
